443ae474b882657226021e430d13b2e837b28763
Balazs Ercsey <laze@laze.hu>
2024-01-09 15:04:45 +0100

    Update base configs.

--- config/neon-admin.php
+++ config/neon-admin.php
@@ -9,7 +9,12 @@ return [
    * 
    * @see https://filamentphp.com/docs/3.x/panels/configuration#changing-the-path
    */
-  'path'  => env('NEON_ADMIN_PATH', 'admin'),
+  'path'      => env('NEON_ADMIN_PATH', 'admin'),
+
+  'groups'    => [
+    'Cica',
+    'Micw'
+  ],
 
   /** Set up folder for admin resources. As you generate resources, please take
    * attention, Neon Admin uses 'App\Admin\Resources' namespace for your resources,
@@ -20,7 +25,9 @@ return [
    * 
    * @see https://filamentphp.com/docs/3.x/panels/resources/getting-started#specifiying-a-custom-model-namespace
    */
-  'resources' => ['Admin/Resources'],
+  'resources' => [
+    'Admin/Resources'
+  ],
 
   /** By default, Filament will respond to requests from all domains. If you'd
    * like to scope it to a specific domain, you can use the domain() method,
@@ -28,18 +35,18 @@ return [
    * 
    * @see https://filamentphp.com/docs/3.x/panels/configuration#setting-a-domain
    */
-  'domain'  => null,
+  'domain'    => null,
 
   /** Neon Admin uses separated guard for authentication and authorization.
    *
    */
-  'guard' => 'admin',
+  'guard'     => 'admin',
 
   /** Admin colors.
    * 
    * @see https://filamentphp.com/docs/3.x/panels/themes#changing-the-colors
    */
-  'colors'  => [
+  'colors'    => [
     'primary'   => '#3b0764',
     'danger'    => '#991b1b',
     'gray'      => '#d8b4fe',
