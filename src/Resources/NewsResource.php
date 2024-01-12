fdd9c5f904b62dbb3d705230136cf036196f5bb1
Balazs Ercsey <laze@laze.hu>
2024-01-12 16:51:58 +0100

    Add News admin. - Neon\News package required.

---
+++ src/Resources/NewsResource.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Neon\Admin\Resources;
+
+use Neon\Admin\Resources\NewsResource\Pages;
+use Neon\Admin\Resources\NewsResource\RelationManagers;
+use Neon\News\Models\News;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NewsResource extends Resource
+{
+    protected static ?string $model = News::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('header-file')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('header-name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('header-type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('header-size')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('lead')
+                    ->required()
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('content')
+                    ->required()
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(1)
+                    ->default('N'),
+                Forms\Components\DateTimePicker::make('published_at'),
+                Forms\Components\DateTimePicker::make('expired_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('header-file')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('header-name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('header-type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('header-size')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('expired_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNews::route('/'),
+            'create' => Pages\CreateNews::route('/create'),
+            'view' => Pages\ViewNews::route('/{record}'),
+            'edit' => Pages\EditNews::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ src/Resources/NewsResource/Pages/ListNews.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Neon\Admin\Resources\NewsResource\Pages;
+
+use Neon\Admin\Resources\NewsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListNews extends ListRecords
+{
+    protected static string $resource = NewsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
