75abbe1bfce2878ff885f2c01627098b32177bca
Balazs Ercsey <laze@laze.hu>
2024-04-02 15:58:17 +0200

    Update NeonAdminServiceProvider.php

--- src/NeonAdminServiceProvider.php
+++ src/NeonAdminServiceProvider.php
@@ -21,7 +21,7 @@ use Spatie\LaravelPackageTools\Package;
 
 class NeonAdminServiceProvider extends PackageServiceProvider
 {
-  const VERSION = '3.0.0-alpha-23';
+  const VERSION = '3.0.0-alpha-24';
 
   public function configurePackage(Package $package): void
   {
